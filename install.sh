55a36de000dc44f5aca2cc439ba894d5170c7508
Piere Wöhl <pierew@users.noreply.github.com>
2015-05-30 17:52:56 +0200

    Update install.sh
    Add CHMOD to Files

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Install Mango"
 cp ./mango /bin/mango
+chmod +x /bin/mango
 echo "Replace trkmenu"
 cp ./trkmenu /bin/trkmenu
+chmod +x /bin/trkmenu
 echo "Replace trkbootnet"
 cp ./trkbootnet /bin/trkbootnet
+chmod +x /bin/trkbootnet
